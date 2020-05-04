573de325bf835d05f885d96466bfc41fa079e6f5
Marcelo Maia <mmaia.cc@gmail.com>
2020-05-04 12:57:52 -0300

    resolução de bug de dependencias

--- src/views/dark-template/content/Container.vue
+++ src/views/dark-template/content/Container.vue
@@ -44,7 +44,11 @@
             <v-flex v-if="skill.divider" :key="i" md12 xs12 mb-4 />
             <v-flex v-else :key="i" md6 xs12>
               <div class="mr-2 ml-2">
-                <div class="align-center">
+                <div v-if="skill.custom" class="align-center">
+                  <span class="iconify" :data-icon="skill.icon"/>
+                  {{ skill.title }}
+                </div>
+                <div v-else class="align-center">
                   <v-icon small>
                     {{ skill.icon }}
                   </v-icon>
@@ -115,14 +119,39 @@ export default {
         icon: "mdi-language-javascript",
         value: 90,
       },
-       {
+      {
         title: "HTML",
         icon: "mdi-language-html5",
         value: 100,
       },
-       {
+      {
         title: "Postgres",
-        icon: "mdi-postgres",
+        icon: "cib:postgresql",
+        custom: true,
+        value: 100,
+      },
+      {
+        title: "MongoDB",
+        icon: "cib:mongodb",
+        custom: true,
+        value: 100,
+      },
+      {
+        title: "Redis",
+        icon: "cib:redis",
+        custom: true,
+        value: 100,
+      },
+      {
+        title: "Nginx",
+        icon: "cib:nginx",
+        custom: true,
+        value: 100,
+      },
+      {
+        title: "Sentry",
+        icon: "cib:sentry",
+        custom: true,
         value: 100,
       },
       {
@@ -132,49 +161,65 @@ export default {
       },
       {
         title: "DART",
-        icon: "mdi-language-dart",
-        value: 60,
+        icon: "simple-icons:dart",
+        custom: true,
+        value: 100
       },
       //TODO: colocar icone django
       {
         title: "Django",
-        icon: "mdi-language-django",
-        value: 90,
+        icon: "cib:django",
+        custom: true,
+        value:100
       },
       {
-        title: "DRF",
-        icon: "mdi-language-django",
-        value: 90,
+        title: "Django Rest Framework",
+        icon: "cib:django",
+        custom: true,
+        value:100
       },
       {
         title: "Pytest",
-        icon: "mdi-language-django",
-        value: 90,
+        icon: "mdi-test-tube",
+        custom: true,
+        value: 100
       },
       {
         title: "Jest",
-        icon: "mdi-language-django",
-        value: 90,
+        icon: "cib:jest",
+        custom: true,
       },
       {
         title: "Flask",
-        icon: "mdi-flask",
+        icon: "cib:flask",
+        value: 90,
+        custom:true
+      },
+      {
+        title: "Material Design",
+        icon: "simple-icons:material-ui",
+        value: 90,
+        custom:true
+      },
+      {
+        title: "Bootstrap 4",
+        icon: "mdi-bootstrap",
         value: 90,
       },
       {
-        title: "Vue.js Framework",
+        title: "Vue.js",
         icon: "mdi-vuejs",
         value: 80,
       },
-            {
-        title: "React.js Framework",
+      {
+        title: "React.js",
         icon: "mdi-react",
         value: 80,
       },
       {
         title: "Flutter",
-        icon: "mdi-language-flutter",
-        value: 80,
+        icon: "cib:flutter",
+        custom: true,
       },
       { divider: true },
       {
@@ -188,9 +233,16 @@ export default {
         value: 70,
       },
       {
-        title: "ExtremeProgramming",
-        icon: "mdi-xp",
+        title: "Extreme Programming",
+        icon: "cryptocurrency:xp",
         value: 70,
+        custom: true
+      },
+      {
+        title: "Domain Driven Design",
+        icon: "whh:business",
+        value: 70,
+        custom: true
       },
       {
         title: "Web Application Security",
@@ -236,5 +288,8 @@ export default {
 }
 .progress {
   margin-top: 0.1rem;
+}
+.iconify{
+  color:#777777
 }
 </style>
